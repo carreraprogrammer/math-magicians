b00831fbaa331de79beb566d7ef5b8b9cf2419f1
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2022-11-02 13:59:29 -0500

    import calculator in the main component and add styles

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -15,22 +15,22 @@ class Calculator extends React.Component {
           <button type="button" className="button">AC</button>
           <button type="button" className="button">+/-</button>
           <button type="button" className="button">%</button>
-          <button type="button" className="button">+</button>
+          <button type="button" className="button orange">÷</button>
           <button type="button" className="button">7</button>
           <button type="button" className="button">8</button>
           <button type="button" className="button">9</button>
-          <button type="button" className="button">x</button>
+          <button type="button" className="button orange">x</button>
           <button type="button" className="button">4</button>
           <button type="button" className="button">5</button>
           <button type="button" className="button">6</button>
-          <button type="button" className="button">-</button>
+          <button type="button" className="button orange">-</button>
           <button type="button" className="button">1</button>
           <button type="button" className="button">2</button>
           <button type="button" className="button">3</button>
-          <button type="button" className="button">+</button>
-          <button type="button" className="button">0</button>
+          <button type="button" className="button orange">+</button>
+          <button type="button" className="button zero">0</button>
           <button type="button" className="button">.</button>
-          <button type="button" className="button">=</button>
+          <button type="button" className="button orange">=</button>
         </div>
       </section>
     );
---
+++ src/index.js
@@ -0,0 +1,12 @@
+/* eslint-disable linebreak-style */
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './styles.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
