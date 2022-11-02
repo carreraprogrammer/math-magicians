dd65df6e94a0b07806f5c48d15c8523e784844ae
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2022-11-02 11:47:50 -0500

    create React.js to match the design

---
+++ src/components/Calculator.js
@@ -0,0 +1,40 @@
+/* eslint-disable linebreak-style */
+import React from 'react';
+
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <section className="calculator-container">
+        <h3 className="result-screen">0</h3>
+        <div className="calculator-keyboard">
+          <button type="button" className="button">AC</button>
+          <button type="button" className="button">+/-</button>
+          <button type="button" className="button">%</button>
+          <button type="button" className="button">+</button>
+          <button type="button" className="button">7</button>
+          <button type="button" className="button">8</button>
+          <button type="button" className="button">9</button>
+          <button type="button" className="button">x</button>
+          <button type="button" className="button">4</button>
+          <button type="button" className="button">5</button>
+          <button type="button" className="button">6</button>
+          <button type="button" className="button">-</button>
+          <button type="button" className="button">1</button>
+          <button type="button" className="button">2</button>
+          <button type="button" className="button">3</button>
+          <button type="button" className="button">+</button>
+          <button type="button" className="button">0</button>
+          <button type="button" className="button">.</button>
+          <button type="button" className="button">=</button>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Calculator;
