41b4568cfb5fa380674328d881b4c539400aeb3d
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2022-11-03 12:12:39 -0500

    add hanclecklick event to each button

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -29,25 +29,25 @@ class Calculator extends React.Component {
           {next}
         </h3>
         <div className="calculator-keyboard">
-          <button type="button" className="button">AC</button>
-          <button type="button" className="button">+/-</button>
-          <button type="button" className="button">%</button>
-          <button type="button" className="button orange">÷</button>
-          <button type="button" className="button">7</button>
-          <button type="button" className="button">8</button>
-          <button type="button" className="button">9</button>
-          <button type="button" className="button orange">x</button>
-          <button type="button" className="button">4</button>
-          <button type="button" className="button">5</button>
-          <button type="button" className="button">6</button>
-          <button type="button" className="button orange">-</button>
-          <button type="button" className="button">1</button>
-          <button type="button" className="button">2</button>
-          <button type="button" className="button">3</button>
-          <button type="button" className="button orange">+</button>
-          <button type="button" className="button zero">0</button>
-          <button type="button" className="button">.</button>
-          <button type="button" className="button orange">=</button>
+          <button type="button" className="button" onClick={this.handleClick}>AC</button>
+          <button type="button" className="button" onClick={this.handleClick}>+/-</button>
+          <button type="button" className="button" onClick={this.handleClick}>%</button>
+          <button type="button" className="button orange" onClick={this.handleClick}>÷</button>
+          <button type="button" className="button" onClick={this.handleClick}>7</button>
+          <button type="button" className="button" onClick={this.handleClick}>8</button>
+          <button type="button" className="button" onClick={this.handleClick}>9</button>
+          <button type="button" className="button orange" onClick={this.handleClick}>x</button>
+          <button type="button" className="button" onClick={this.handleClick}>4</button>
+          <button type="button" className="button" onClick={this.handleClick}>5</button>
+          <button type="button" className="button" onClick={this.handleClick}>6</button>
+          <button type="button" className="button orange" onClick={this.handleClick}>-</button>
+          <button type="button" className="button" onClick={this.handleClick}>1</button>
+          <button type="button" className="button" onClick={this.handleClick}>2</button>
+          <button type="button" className="button" onClick={this.handleClick}>3</button>
+          <button type="button" className="button orange" onClick={this.handleClick}>+</button>
+          <button type="button" className="button zero" onClick={this.handleClick}>0</button>
+          <button type="button" className="button" onClick={this.handleClick}>.</button>
+          <button type="button" className="button orange" onClick={this.handleClick}>=</button>
         </div>
       </section>
     );
