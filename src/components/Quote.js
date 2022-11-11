dcba323d8ab288ad8e396d7ecf1aaa718826f76a
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2022-11-10 23:39:36 -0500

    basic style for quotes component

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,5 +1,9 @@
+/* eslint-disable max-len */
 const Quote = () => (
-  <h1> </h1>
+  <div className="quoteContainer">
+    <h4 className="quote">&quot;Without mathematics, there is nothing you can do. Everything around you is mathematics. Everything around you is numbers.&quot;</h4>
+    <p className="author">- Shakuntala Devi, Indian writer and mental calculator</p>
+  </div>
 );
 
 export default Quote;
