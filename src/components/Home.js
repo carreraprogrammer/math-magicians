0efcdabaaccaeb7e2faf3ec2d0912444e163dd05
carreraprogrammer <109081622+carreraprogrammer@users.noreply.github.com>
2022-11-10 23:24:34 -0500

    Add basic styles for Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,21 @@
+/* eslint-disable max-len */
 const Home = () => (
-  <h1> </h1>
+  <section className="home">
+    <h2 className="homeTitle">Welcome to our page</h2>
+    <p className="homePar">
+      {' '}
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam nulla culpa voluptate minus necessitatibus vitae, porro placeat ad assumenda rem eum repellat labore molestiae ipsum error. Harum soluta alias fugiat.
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam nulla culpa voluptate minus necessitatibus vitae, porro placeat ad assumenda rem eum repellat labore molestiae ipsum error. Harum soluta alias fugiat. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam nulla culpa voluptate minus necessitatibus vitae, porro placeat ad assumenda rem eum repellat labore molestiae ipsum error. Harum soluta alias fugiat. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam nulla culpa voluptate minus necessitatibus vitae, porro placeat ad assumenda rem eum repellat labore molestiae ipsum error. Harum soluta alias fugiat.
+
+    </p>
+    <p className="homePar">
+      {' '}
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam nulla culpa voluptate minus necessitatibus vitae, porro placeat ad assumenda rem eum repellat labore molestiae ipsum error. Harum soluta alias fugiat.
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam nulla culpa voluptate minus necessitatibus vitae, porro placeat ad assumenda rem eum repellat labore molestiae ipsum error. Harum soluta alias fugiat. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam nulla culpa voluptate minus necessitatibus vitae, porro placeat ad assumenda rem eum repellat labore molestiae ipsum error. Harum soluta alias fugiat. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam nulla culpa voluptate minus necessitatibus vitae, porro placeat ad assumenda rem eum repellat labore molestiae ipsum error. Harum soluta alias fugiat.
+
+    </p>
+  </section>
+
 );
 
 export default Home;
